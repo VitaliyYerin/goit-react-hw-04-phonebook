f5c7992f754d77cce84e089faf04130b0d21df98
Vitaliy Yerin <37743681+VitaliyYerin@users.noreply.github.com>
2022-08-24 18:01:40 +0300

    change Contact List

---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import s from './ContactList.module.css';
+
+const ContactList = ({ contacts, onDeleteContact }) => {
+  return (
+    <ul className={s.listContacts}>
+      {contacts.map(({ id, name, number }) => (
+        <li key={id} className={s.listItem}>
+          <p>
+            {name}........ {number}
+          </p>
+          <button type="submit" onClick={() => onDeleteContact(id)}>
+            Delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+    })
+  ),
+  onDeleteContact: PropTypes.func,
+};
+
+export default ContactList;
